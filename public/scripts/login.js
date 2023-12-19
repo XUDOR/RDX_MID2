6e20e80fa3e9ff10acec5f60dbd6f3c2fafb46d2
Roderick Shoolbraid <rdxenv@gmail.com>
2023-12-19 15:07:27 -0800

    working on login/ user creation

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,10 +1,59 @@
-document.getElementById('loginForm').addEventListener('submit',function(e){
+document.getElementById('loginForm').addEventListener('submit',function(e) {
   e.preventDefault();
   
   let username = document.getElementById('username').value;
   let password = document.getElementById('password').value;
 
-  // to do:::  AJAX request to server
+  const data = {
+    username: username,
+    password: password
+  };
 
-console.log("Login form submitted", username);
+  document.getElementById('loginForm').addEventListener('submit',function(e) {
+    e.preventDefault();
+    
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+  
+    const data = {
+      username: username,
+      password: password
+    };
+  
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success', data);
+      })
+  
+      .catch((error) => {
+        console.error('Error', error);
+      });
+  
+    console.log("Login form submitted", username);
+  });
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success', data);
+    })
+
+    .catch((error) => {
+      console.error('Error', error);
+    });
+
+  console.log("Login form submitted", username);
 });
